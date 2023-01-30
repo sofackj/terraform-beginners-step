d4068c3b02963e5213b4176a8519045aa7d4db11
Julien <sofa.kreutz@yahoo.fr>
2023-01-30 13:57:35 +0100

    change hostnames

--- ec2.tf
+++ ec2.tf
@@ -13,17 +13,31 @@ resource "aws_instance" "k8s-controler" {
     Name = "k8s-controller"
   }
 }
-resource "aws_instance" "k8s-worker" {
+resource "aws_instance" "k8s-worker-1" {
    instance_type = "t2.micro"
    ami = var.ami_id
-   count = 2
    subnet_id = aws_subnet.robin.id
    # Apply security groups to the instance
    vpc_security_group_ids = [
     aws_security_group.allow_all.id,
    ]
    # Execute the script when starting the instance
-   user_data = file("./script-k8s-worker.sh")
+   user_data = file("./script-k8s-worker-1.sh")
+   
+   tags = {
+    Name = "k8s-worker"
+  }
+}
+resource "aws_instance" "k8s-worker-2" {
+   instance_type = "t2.micro"
+   ami = var.ami_id
+   subnet_id = aws_subnet.robin.id
+   # Apply security groups to the instance
+   vpc_security_group_ids = [
+    aws_security_group.allow_all.id,
+   ]
+   # Execute the script when starting the instance
+   user_data = file("./script-k8s-worker-2.sh")
    
    tags = {
     Name = "k8s-worker"
