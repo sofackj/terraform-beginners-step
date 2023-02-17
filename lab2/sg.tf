50a9c2ebcfcb823f205e8aac78b10598630fc730
Julien <sofa.kreutz@yahoo.fr>
2023-02-17 15:06:56 +0100

    Add providers.tf and few updates

--- lab2/sg.tf
+++ lab2/sg.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "sgpubec2" {
     from_port        = 80
     to_port          = 80
     protocol         = "tcp"
-    cidr_blocks      = [aws_security_group.sgpublb.id]
+    security_groups  = [aws_security_group.sgpublb.id]
   }
 
   ingress {
