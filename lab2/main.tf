18b334a6b211a24d591723f646aa1a6ad07a4525
Julien <sofa.kreutz@yahoo.fr>
2023-02-17 12:48:08 +0100

    Add providers.tf and few updates

--- lab2/main.tf
+++ lab2/main.tf
@@ -1,10 +1,7 @@
 provider "aws" {
  region = "eu-west-3"
  profile = "default"
- access_key = var.access_key
- secret_key = var.secret_key
 }
-
 terraform {
  backend "s3" {
  bucket = "nuumfactory-terraform-backend"
