646fa9397f35cc05cd3515836cc0643d3ad6341e
Julien <sofa.kreutz@yahoo.fr>
2023-02-17 12:33:13 +0100

    Add providers.tf and few updates

--- appWandja/main.tf
+++ appWandja/main.tf
@@ -1,7 +1,5 @@
-terraform {
- backend "s3" {
- bucket = "nuumfactory-terraform-backend"
- key = "terraform-15.tfstate"
- region = "eu-west-3"
- }
+backend "s3" {
+bucket = "nuumfactory-terraform-backend"
+key = "terraform-15.tfstate"
+region = "eu-west-3"
 }
