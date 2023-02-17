3e38f687033a0ff8078afb1b8a0d5751092dca08
Julien <sofa.kreutz@yahoo.fr>
2023-02-17 13:16:39 +0100

    Add providers.tf and few updates

---
+++ labtest/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+ region = "eu-west-3"
+ access_key = var.access_key
+ secret_key = var.secret_key
+}
+
+backend "s3" {
+bucket = "nuumfactory-terraform-backend"
+key = "terraform-15.tfstate"
+region = "eu-west-3"
+}
