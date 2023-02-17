6a44b3d948a0c43dcff6fe225c48f29c65efb7f6
Julien <sofa.kreutz@yahoo.fr>
2023-02-17 15:39:18 +0100

    Add providers.tf and few updates

--- lab2/network.tf
+++ lab2/network.tf
@@ -1,41 +1,41 @@
-# Create a VPC
-# resource "aws_vpc" "vpc1" {
-#   cidr_block       = "13.57.128.0/17"
-#   instance_tenancy = "default"
+# # Create a VPC
+# # resource "aws_vpc" "vpc1" {
+# #   cidr_block       = "13.57.128.0/17"
+# #   instance_tenancy = "default"
+
+# #   tags = {
+# #     owner =  "jsofack-lemaire@thenuumfactory.fr"
+# #     ephemere = "oui"
+# #     entity =  "numfactory"
+# #   }
+# # }
+# # Attach an internet gateway to the vpc "main"
+# # resource "aws_internet_gateway" "gw" {
+# #   vpc_id = aws_vpc.vpc1.id
+
+# #   tags = {
+# #     owner =  "jsofack-lemaire@thenuumfactory.fr"
+# #     ephemere = "oui"
+# #     entity =  "numfactory"
+# #   }
+# # }
+# resource "aws_subnet" "publicsubnet" {
+#   vpc_id                  = var.vpc_id
+#   cidr_block              = "10.0.111.128/25"
+#   availability_zone       = "eu-west-3b"
+#   map_public_ip_on_launch = true
 
 #   tags = {
-#     owner =  "jsofack-lemaire@thenuumfactory.fr"
-#     ephemere = "oui"
-#     entity =  "numfactory"
+#     Name = "nuumfactory-dev-db-subpub-15"
 #   }
 # }
-# Attach an internet gateway to the vpc "main"
-# resource "aws_internet_gateway" "gw" {
-#   vpc_id = aws_vpc.vpc1.id
+# # Add a private subnet in the vpc "main"
+# resource "aws_subnet" "privatesubnet" {
+#   vpc_id                  = var.vpc_id
+#   cidr_block              = "10.0.111.0/25"
+#   availability_zone       = "eu-west-3b"
 
 #   tags = {
-#     owner =  "jsofack-lemaire@thenuumfactory.fr"
-#     ephemere = "oui"
-#     entity =  "numfactory"
+#     Name = "nuumfactory-dev-db-subpriv-15"
 #   }
 # }
-resource "aws_subnet" "publicsubnet" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.111.128/25"
-  availability_zone       = "eu-west-3b"
-  map_public_ip_on_launch = true
-
-  tags = {
-    Name = "nuumfactory-dev-db-subpub-15"
-  }
-}
-# Add a private subnet in the vpc "main"
-resource "aws_subnet" "privatesubnet" {
-  vpc_id                  = var.vpc_id
-  cidr_block              = "10.0.111.0/25"
-  availability_zone       = "eu-west-3b"
-
-  tags = {
-    Name = "nuumfactory-dev-db-subpriv-15"
-  }
-}
--- lab2/rtb.tf
+++ lab2/rtb.tf
@@ -1,29 +1,29 @@
-# Create route table for public subnets
-resource "aws_route_table" "public" {
-  vpc_id = var.vpc_id
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = var.igw_id
-  }
-  tags = {
-    Name = "nuumfactory-dev-rtb-pub-15"
-  }
-}
-# Create route table for private subnets
-resource "aws_route_table" "private" {
-  vpc_id = var.vpc_id
-  tags = {
-    Name = "nuumfactory-dev-rtb-pub-15"
-  }
-}
-# Associate the rtb "harley" to public subnets
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.publicsubnet.id
-  route_table_id = aws_route_table.public.id
-}
-# Associate the rtb "quinn" to private subnets
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.privatesubnet.id
-  route_table_id = aws_route_table.private.id
-}
+# # Create route table for public subnets
+# resource "aws_route_table" "public" {
+#   vpc_id = var.vpc_id
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = var.igw_id
+#   }
+#   tags = {
+#     Name = "nuumfactory-dev-rtb-pub-15"
+#   }
+# }
+# # Create route table for private subnets
+# resource "aws_route_table" "private" {
+#   vpc_id = var.vpc_id
+#   tags = {
+#     Name = "nuumfactory-dev-rtb-pub-15"
+#   }
+# }
+# # Associate the rtb "harley" to public subnets
+# resource "aws_route_table_association" "public" {
+#   subnet_id      = aws_subnet.publicsubnet.id
+#   route_table_id = aws_route_table.public.id
+# }
+# # Associate the rtb "quinn" to private subnets
+# resource "aws_route_table_association" "private" {
+#   subnet_id      = aws_subnet.privatesubnet.id
+#   route_table_id = aws_route_table.private.id
+# }
 
