5ddfc5dcb7c64d2ea98d8b79d15d16faf6087304
Julien <sofa.kreutz@yahoo.fr>
2023-02-20 13:38:49 +0100

    Add providers.tf and few updates

--- lab2/script-apache.sh
+++ lab2/script-apache.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo hostnamectl hostname hello-world
-sudo -i <<EOF
-apt-get update -y
-apt-get install apache2 -y
-EOF
+yum update -y
+amazon-linux-extras install php8.0 mariadb10.5
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
