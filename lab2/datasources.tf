68791723592a8eb8bcde01a854262ee73c864db0
Julien <sofa.kreutz@yahoo.fr>
2023-02-20 12:31:07 +0100

    Add providers.tf and few updates

--- lab2/datasources.tf
+++ lab2/datasources.tf
@@ -3,13 +3,15 @@ data "aws_vpc" "vpc" {
     Name = var.vpc
   }
 }
-# data "aws_subnet" "subpub1" {
-#   tags = {
-#     Name = var.elb_subnets[0]
-#   }
-# }
-# data "aws_subnet" "subpub2" {
-#   tags = {
-#     Name = var.elb_subnets[1]
-#   }
-# }
+data "aws_subnet" "subpub1" {
+  filter {
+    name   = "tag:Name"
+    values = [var.elb_subnets[0]]
+  }
+}
+data "aws_subnet" "subpub2" {
+  filter {
+    name   = "tag:Name"
+    values = [var.elb_subnets[1]]
+  }
+}
