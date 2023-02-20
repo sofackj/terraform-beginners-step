8d6a27be9540d27e1775fc4f585b931f20d7e2ed
Julien <sofa.kreutz@yahoo.fr>
2023-02-20 12:22:14 +0100

    Add providers.tf and few updates

--- lab2/datasources.tf
+++ lab2/datasources.tf
@@ -3,13 +3,13 @@ data "aws_vpc" "vpc" {
     Name = var.vpc
   }
 }
-data "aws_subnet" "subpub1" {
-  tags = {
-    Name = var.elb_subnets[0]
-  }
-}
-data "aws_subnet" "subpub2" {
-  tags = {
-    Name = var.elb_subnets[1]
-  }
-}
+# data "aws_subnet" "subpub1" {
+#   tags = {
+#     Name = var.elb_subnets[0]
+#   }
+# }
+# data "aws_subnet" "subpub2" {
+#   tags = {
+#     Name = var.elb_subnets[1]
+#   }
+# }
