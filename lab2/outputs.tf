095f8ce39bf358d063d02034dfee6c941c2a6129
Julien <sofa.kreutz@yahoo.fr>
2023-02-20 14:22:24 +0100

    Add providers.tf and few updates

--- lab2/outputs.tf
+++ lab2/outputs.tf
@@ -1,6 +1,3 @@
-output "instance_ip_addr" {
-  value = aws_instance.appserver.public_ip
-}
 output "ssh_command" {
     value = "ssh -i ec2-private-key.pem ec2-user@${aws_instance.appserver.public_ip}"
 }
