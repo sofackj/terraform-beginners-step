f62b0293443d246f834487f7ff7e4e0589b3b548
Julien <sofa.kreutz@yahoo.fr>
2023-02-21 14:13:53 +0100

    Add providers.tf and few updates

--- labmini/main.tf
+++ labmini/main.tf
@@ -25,6 +25,7 @@ resource "aws_instance" "ec2hello" {
   ami = "ami-0f15e0a4c8d3ee5fe"
   instance_type = "t2.micro"
   subnet_id = "subnet-0671910eaa06d0c87"
+  associate_public_ip_address = true
   vpc_security_group_ids = [
     aws_security_group.sglambda.id,
    ]
