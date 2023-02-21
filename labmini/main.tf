95b4876b10ff73bc2452fd78ca4d4afec691a8b5
Julien <sofa.kreutz@yahoo.fr>
2023-02-21 09:41:06 +0100

    Add providers.tf and few updates

--- labmini/main.tf
+++ labmini/main.tf
@@ -1,20 +1,12 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "nuumfactory-s3-import-15"
+  count = 5
+  bucket = "nuumfactory-s3-count-15-${count.index}"
+  tags = {
+    count-index = "${count.index}"
+  }
 }
 resource "aws_s3_bucket_versioning" "versioning_example" {
   bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-resource "aws_s3_bucket" "bucket2" {
-  bucket = "nuumfactory-s3-import-2-15"
-  tags = {
-    imported = "true"
-  }
-}
-resource "aws_s3_bucket_versioning" "versioning_example2" {
-  bucket = aws_s3_bucket.bucket2.id
   versioning_configuration {
     status = "Enabled"
   }
