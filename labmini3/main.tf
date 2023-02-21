66d753ecbfebb928dce4d25cd23479b6be8d27e9
Julien <sofa.kreutz@yahoo.fr>
2023-02-21 16:18:23 +0100

    Add providers.tf and few updates

--- labmini3/main.tf
+++ labmini3/main.tf
@@ -1,3 +1,20 @@
-resource "aws_s3_bucket" "bucket" {
-  bucket = "nuumfactory-s3-taint-15" 
+resource "aws_launch_template" "example" {
+  name_prefix   = "example"
+  image_id      = "ami-0f15e0a4c8d3ee5fe"
+  instance_type = var.environment == "dev" ? "t2.micro" : "t2.small"
+}
+resource "aws_autoscaling_group" "hello" {
+  name                      = " nuumfactory-asg-conditions-15"
+  availability_zones = ["eu-west-3a","eu-west-3b","eu-west-3c"]
+  min_size           = var.environment == "dev" ? 1 : 2
+  max_size           = var.environment == "dev" ? 2 : 4
+
+  mixed_instances_policy {
+    launch_template {
+      launch_template_specification {
+        launch_template_id = aws_launch_template.example.id
+      }
+    }
+  }
+
 }
