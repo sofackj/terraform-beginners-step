ba284ae330eb587f80c137e2cc5bf15b41837ad1
Julien <sofa.kreutz@yahoo.fr>
2023-02-21 16:04:50 +0100

    Add providers.tf and few updates

--- labmini3/main.tf
+++ labmini3/main.tf
@@ -1,20 +1,3 @@
 resource "aws_s3_bucket" "bucket" {
-  bucket = "nuumfactory-s3-null-resource-15"
-  
+  bucket = "nuumfactory-s3-taint-15" 
 }
-
-resource "aws_s3_bucket_versioning" "example" {
-  bucket = aws_s3_bucket.bucket.id
-  versioning_configuration {
-    status = "Suspended"
-  }
-}
-resource "null_resource" "toto" {
-    triggers = {
-      status = aws_s3_bucket_versioning.example.versioning_configuration[0].status
-    }
-    provisioner "local-exec" {
-      command = "echo 'Statut du versioning : ${aws_s3_bucket_versioning.example.versioning_configuration[0].status}' > test.txt"
-    }
-  }
-
