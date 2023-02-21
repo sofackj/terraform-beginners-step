f289897f5752fef90121586a81c3be56154e259d
Julien <sofa.kreutz@yahoo.fr>
2023-02-21 14:05:03 +0100

    Add providers.tf and few updates

--- labmini/main.tf
+++ labmini/main.tf
@@ -1,18 +1,37 @@
-resource "aws_instance" "ec2" {
-    for_each = {
-        "instance1" = {"type" = "t2.micro","az" = "a","public_ip" = true}
-        "instance2" = {"type" = "t2.small","az" = "b","public_ip" = true}
-        "instance3" = {"type" = "t2.medium","az" = "c","public_ip" = true}
-        "instance4" = {"type" = "t2.large","az" = "a","public_ip" = false}
-        "instance5" = {"type" = "t2.xlarge","az" = "b","public_ip" = false}
-    }
+resource "aws_security_group" "sglambda" {
+  description = "ec2-sg"
+  vpc_id      = "vpc-06b654aaaf2422e1f"
 
-    ami = "ami-0f15e0a4c8d3ee5fe"
-    associate_public_ip_address = each.value.public_ip
-    instance_type = "${each.value.type}"
-    availability_zone = "eu-west-3${each.value.az}"
+  ingress {
+    from_port        = -1
+    to_port          = -1
+    protocol         = "icmp"
+    cidr_blocks      = ["5.50.34.152/32"]
+  }
 
-    tags = {
-         Name = "nuumfactory-ec2-for-each-15"
-    }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "nuumfactory-sg-local-exec-15"
+  }
+}
+
+resource "aws_instance" "ec2hello" {
+  ami = "ami-0f15e0a4c8d3ee5fe"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0671910eaa06d0c87"
+  vpc_security_group_ids = [
+    aws_security_group.sglambda.id,
+   ]
+  tags = {
+    Name = "nuumfactory-ec2-local-exec-15"
+  }
+  provisioner "local-exec" {
+    command = "ping ${self.public_ip} >> ping.txt"
+  }
 }
--- labmini/providers.tf
+++ labmini/providers.tf
@@ -10,17 +10,3 @@ provider "aws" {
  region = "eu-west-3"
  profile = "default"
 }
-# provider "aws" {
-#   alias = "united"
-#   region = "us-east-1"
-# }
-# provider "aws" {
-#   alias = "nordic"
-#   region = "eu-north-1"
-# }
-# provider "aws" {
-#   alias = "asia"
-#   region = "ap-south-1"
-# }
-
-
